f0e1f8dc04d8542b068f3aab3c0aa499efa2ab2d
Evi <evicoach@gmail.com>
2019-12-18 08:21:22 +0100

    Successfully added the controls for either increasing or decreasing the Burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,28 @@ import React from 'react'
 import classes from './Burger.module.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
-const burger = () => {
+const burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(ingredientsKey => {
+            return [
+                // return an array with length equivalent
+                // to the number of ingredients for this key
+                ...Array(props.ingredients[ingredientsKey])
+            ].map((_, index) => {
+                return <BurgerIngredient key={ingredientsKey + index}
+                    type={ingredientsKey} />
+            })
+        }).reduce((arr, el) => {
+            return arr.concat(el)
+        }, [])
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients</p>
+    }
+    console.log(transformedIngredients)
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     )
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,17 +2,27 @@ import React, { Component } from 'react'
 
 import Auxiliary from '../../hoc/Auxiliary'
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
-class BurgerBuilder extends Component{
+class BurgerBuilder extends Component {
 
-    constructor(props) {
-        super(props)
+
+
+    state = {
+        ingredient: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0,
+        }
     }
+
 
     render() {
         return (
             <Auxiliary>
-                <Burger />
+                <Burger ingredients={this.state.ingredient} />
+                <BuildControls />
             </Auxiliary>
         )
     }
